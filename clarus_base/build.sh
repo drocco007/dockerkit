f1b703fc635570bee27ba55a17b2182bb55d9137
Daniel Rocco <drocco@gmail.com>
2016-03-23 19:20:51 -0400

    Parameterize clarus_base, remove pip cache

--- clarus_base/build.sh
+++ clarus_base/build.sh
@@ -1,23 +1,28 @@
 NAME="clarus_base"
 SOURCE_ROOT=$HOME/source/brightlink
-PIP_DOWNLOAD_CACHE=$HOME/.cache/pip
 
 
 docker rm $NAME
 docker build -t "$NAME" .
 
+
 docker run -i --name "$NAME" \
-  -v "$SOURCE_ROOT:/brightlink_dev" -v "$PIP_DOWNLOAD_CACHE:/home/docker/.cache/pip" -u docker "$NAME" /bin/bash <<'EOF'
+  -v "$SOURCE_ROOT:/brightlink_dev" -u docker "$NAME" /bin/bash <<'EOF'
 
 set -e
 
-export PIP_DOWNLOAD_CACHE=$HOME/.cache/pip
+
+CLARUS_ROOT=$SOURCE_ROOT/clarus
+MODULES_ROOT=$SOURCE_ROOT/modules-git
+FORKS_ROOT=$SOURCE_ROOT/packages/forks
+
+
 PIP="/home/docker/docker_env/bin/pip install --extra-index-url https://devpi.thebrightlink.com/ops/brightlink/+simple/ "
 PYTHON="/home/docker/docker_env/bin/python"
 
-$PIP /brightlink_dev/packages/trml2pdf/trml2pdf-1.0.tar.gz
+$PIP trml2pdf
 
-for package in `find /brightlink_dev/packages/forks -name trunk` ; do
+for package in `find $FORKS_ROOT -name trunk` ; do
     cd $package
 
     # not develop because it breaks django
@@ -27,19 +32,20 @@ done
 
 
 # Install the base Python packages
-$PIP --allow-all-external --allow-unverified PEAK-Rules --allow-unverified CherryPy --allow-unverified cElementTree --allow-unverified elementtree --allow-unverified pyDes -r /brightlink_dev/brighttrac/requirements.txt
-$PIP -r /brightlink_dev/modules-git/blauthentication/requirements.txt
-$PIP -r /brightlink_dev/modules-git/blcore/requirements.txt
-$PIP -r /brightlink_dev/modules-git/bltemplates/requirements.txt
+$PIP --allow-all-external --allow-unverified PEAK-Rules --allow-unverified CherryPy --allow-unverified cElementTree --allow-unverified elementtree --allow-unverified pyDes -r $CLARUS_ROOT/requirements.txt
+$PIP -r $MODULES_ROOT/blauthentication/requirements.txt
+$PIP -r $MODULES_ROOT/blcore/requirements.txt
+$PIP -r $MODULES_ROOT/bltemplates/requirements.txt
 $PIP pytest pytest-xdist pdbpp django-debug-toolbar==0.9.1
 
 
 $PIP -r /brightlink_dev/satchmo_braintree/requirements.txt
 $PIP -e /brightlink_dev/satchmo_braintree
 
+
 # Install our packages
 for package in blcore blauthentication blconfig blerrorhandling bllang blnotification blexcel blrules blfilter bllocking blscripts blcrypto blintegration blmonitor bltemplates blwebtop utctime template_resolver blpayments switchboard turborest ; do
-    cd /brightlink_dev/modules-git/$package
+    cd $MODULES_ROOT/$package
     $PYTHON setup.py develop
     cd -
 done
